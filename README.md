6b4513084b086e7a66d3825750867fa3dfeea458
almondnick <nickalmond14@gmail.com>
2023-10-12 15:57:16 -0500

    finishing touches to js file and updated README file

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
-# password-generator
+# Password Generator
+
+## Description
+
+This webpage was created to demonstrate Javascript skills by building a password generator using functions, arrays, for loops, variables, and Javascript methods. 
+
+## Installation 
+
+N/A
+
+## Usage
+
+To use this website, click on the "Generate Password" button. The site will take you through a series of prompts asking how long you want the password to be, if you want uppercase letters, lowercase letters, numbers, and special characters to be included. Once you have gone through the prompts, the password will be displayed in the text box.
+
+## Contact
+
+Nick Almond - nickalmond14@gmail.com
+Github Pages Link - https://almondnick.github.io/password-generator/
+Screenshot -
+
+## Credits
+
+Many thanks to Brett Gaynor and Chris Pesar for their guidance on this project. Resources used include Stack Overflow, W3Schools, and MDN WebDocs.
--- script.js
+++ script.js
@@ -1,54 +1,55 @@
-// Assignment Code
-var generateBtn = document.querySelector("#generate");
-
- // Defined all variables needed and the arrays of characters to be used
- var lowerCaseChars = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
- var upperCaseChars = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
- var numberChars = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
- var specialChars = ['!', '@', '#', '$', '%', '&', '(', ')', '*', '+', '-', '.', '/', ':', ';', '<', '>', '?', '[', ']', '^', '_', '{', '}', '~'];
- var passwordArray = [];
-
-// Function to Select Number of Characters
-function numberSelect() {
-    var userInput = window.prompt("How many characters would you like your password to contain?");
+function getPasswordLength() {
+    var userInput = prompt("How many characters would you like your password to be?");
     if (userInput < 8) {
         alert("Your password must be at least 8 characters long.");
+        window.location.reload(true);
     } else if (userInput > 128) {
-        alert("Your password cannot be more than 128 characters long.");
+        alert("Your password cannot exceed 128 characters in length.");
+        window.location.reload(true);
     }
+    return userInput;
 }
 
-// Function to Select Characters Used
-function characterSelect() {    
-    // If then statements determine if characters are added
-    var lowerCase = confirm("Do you want lowercase letters in your password?");
-    if (lowerCase === true) {
-        passwordArray = passwordArray.push(lowerCaseChars);
+function generatePassword() {
+    var lowerCaseChars = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+    var upperCaseChars = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+    var numChars = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+    var specChars = ['!', '@', '#', '$', '%', '&', '(', ')', '*', '+', '-', '.', '/', ':', ';', '<', '>', '?', '[', ']', '^', '_', '{', '}', '~'];
+    var passwordArray = [];
+    var passwordLength = getPasswordLength();
+
+    var lowerCase = confirm("Would you like lowercase letters in your password?");
+    if (lowerCase) {
+        passwordArray = passwordArray.concat(lowerCaseChars);
+    }
+    var upperCase = confirm("Would you like uppercase letters in your password?");
+    if (upperCase) {
+        passwordArray = passwordArray.concat(upperCaseChars);
+    }
+    var numCase = confirm("Would you like numbers in your password?");
+    if (numCase) {
+        passwordArray = passwordArray.concat(numChars);
+    }
+    var specCase = confirm("Would you like special characters in your password?");
+    if (specCase) {
+        passwordArray = passwordArray.concat(specChars);
     }
 
-    var upperCase = confirm("Do you want uppercase letters in your password?");
-    if (upperCase === true) {
-        passwordArray = passwordArray.push(upperCaseChars);
+    var passwordString = "";
+    for (var i = 0; i < passwordLength; i++) {
+        passwordString += passwordArray[Math.floor(Math.random() * (passwordArray.length))];
     }
+    return passwordString;
+}
 
-    var numberCase = confirm("Do you want numbers in your password?");
-    if (numberCase === true) {
-        passwordArray = passwordArray.push(numberChars);
-    }
-
-    var specialCase = confirm("Do you want special characters in your password?");
-    if (specialCase === true) {
-        passwordArray = passwordArray.push(specialChars);
-    }
-} 
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
